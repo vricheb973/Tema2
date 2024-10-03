637fc8e1bcb09e7be940e1d1f2da96cb820d46ac
vricheb973 <vricheb973@g.educaand.es>
2024-10-03 10:44:04 +0200

    Subida inicial

---
+++ ApuntesTema2/src/tema2/Diapositiva07.java
@@ -0,0 +1,24 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class Diapositiva07 {
+
+	public static void main(String[] args) {
+
+		Scanner sca = new Scanner(System.in);
+		
+		System.out.println("Dime un número entero: ");
+		int num1 = sca.nextInt();
+		
+		System.out.println("Dime otro número entero: ");
+		
+		int num2 = sca.nextInt();
+		
+		boolean compro = num1 != num2 || num1 == 0 || num2 == 0;
+		
+		System.out.println(compro);
+
+	}
+
+}
